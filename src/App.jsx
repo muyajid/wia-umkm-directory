56d30c4b5751af941aeaf01f65adea3f39d12718
dira28 <askadira@gmail.com>
2025-10-24 13:21:42 +0700

    [DR]add data UMKM

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
 
   return (
     <h1 className='text-lg-center text-uppercase'>Hello Worlds</h1>
-    <p></p>
   )
 }
 
---
+++ src/data/data-umkm.js
@@ -0,0 +1,155 @@
+[
+  {
+    "id": 1,
+    "nama": "Monroe Petshop Besito",
+    "kategori": ["Toko & Jasa"],
+    "deskripsi": "Petshop lengkap dengan perlengkapan hewan dan layanan perawatan. Cocok buat pemilik hewan peliharaan.",
+    "alamat": "6RXV+35J, Gg. 10, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/94KsWCL3D1WgSNgq9",
+    "gambar": ["monroe1.jpg", "monroe2.jpg"]
+  },
+  {
+    "id": 2,
+    "nama": "Bakso Malvinas",
+    "kategori": ["Makanan", "Minuman"],
+    "deskripsi": "Nikmati bakso legendaris dengan cita rasa gurih dan kuah hangat yang bikin nagih. Cocok buat sarapan atau makan siang santai.",
+    "alamat": "6RWR+JX4, Gg. 10, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/7HhWAPZUdjCkKZHB6",
+    "gambar": ["bakso1.jpg", "bakso2.jpg"]
+  },
+  {
+    "id": 3,
+    "nama": "Habib Tronik",
+    "kategori": ["Toko & Jasa"],
+    "deskripsi": "Toko serba elektronik: pulsa, kabel charger, headphone, dan aksesoris gadget lainnya. Cocok untuk kebutuhan harian elektronikmu.",
+    "alamat": "6RXV+G83, Gg. 10, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/v8EPZLuUG4b4eFUV6",
+    "gambar": ["habib1.jpg", "habib2.jpg"]
+  },
+  {
+    "id": 4,
+    "nama": "Salsa Bakery X Qaismeal Kudus",
+    "kategori": ["Makanan", "Minuman"],
+    "deskripsi": "Rasakan aneka roti dan pastry fresh yang dibuat dengan cinta. Tempat cocok buat nongkrong sambil menikmati cemilan manis.",
+    "alamat": "6RWV+93X, Jl. Bae-Besito, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah",
+    "maps": "https://maps.app.goo.gl/55a428GbbMupHdFZA",
+    "gambar": ["bakery1.jpg", "bakery2.jpg"]
+  },
+  {
+    "id": 5,
+    "nama": "Inti Coffee",
+    "kategori": ["Minuman", "Makanan"],
+    "deskripsi": "Nikmati kopi pilihan dengan suasana nyaman dan seru. Tempat ideal buat kerja, ngobrol, atau sekadar santai.",
+    "alamat": "6RVR+9CF, Jl. Besito - Gebog No.39, RT.2/RW.05, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/J33S4Cj4TQWjXGZNA",
+    "gambar": ["inti1.jpg", "inti2.jpg"]
+  },
+  {
+    "id": 6,
+    "nama": "Ayam Geprek Kremes Tiya Putra",
+    "kategori": ["Makanan", "Minuman"],
+    "deskripsi": "Ayam geprek kremes super renyah dan pedas nikmat. Cocok untuk pecinta kuliner pedas yang selalu ingin lebih!",
+    "alamat": "Gg. 10 No.184, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/K8N63ABmPrNVY1wm9",
+    "gambar": ["tiya1.jpg", "tiya2.jpg"]
+  },
+  {
+    "id": 7,
+    "nama": "TPC2 Coffee",
+    "kategori": ["Makanan", "Minuman"],
+    "deskripsi": "Tempat hangout lengkap dengan menu makan dan minuman. Cocok buat santai sambil menikmati kopi atau camilan ringan.",
+    "alamat": "Jl. Raya PR Sukun, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/EnzE7YPBp25mAYuZ8",
+    "gambar": ["tpc2_1.jpg", "tpc2_2.jpg"]
+  },
+  {
+    "id": 8,
+    "nama": "Rumah Makan Masakan Padang Putri Minang",
+    "kategori": ["Makanan", "Minuman"],
+    "deskripsi": "Nikmati hidangan Padang autentik dengan cita rasa asli Minang. Cocok buat makan siang keluarga atau teman-teman.",
+    "alamat": "6RWR+GQM, Jl. Raya PR Sukun, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/K47Xo3TgK35jENiC6",
+    "gambar": ["putri1.jpg", "putri2.jpg"]
+  },
+  {
+    "id": 9,
+    "nama": "Kedai DPR Coffee",
+    "kategori": ["Minuman", "Makanan"],
+    "deskripsi": "Tempat nongkrong asik dengan kopi nikmat dan suasana cozy. Cocok buat santai sambil ngobrol dengan teman.",
+    "alamat": "Gg. 06, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/9nb63pxiZWWVkzoHA",
+    "gambar": ["dpr1.jpg", "dpr2.jpg"]
+  },
+  {
+    "id": 10,
+    "nama": "Toko Bangunan Ilham Jaya",
+    "kategori": ["Toko & Jasa"],
+    "deskripsi": "Toko bangunan lengkap dengan berbagai material berkualitas. Cocok buat renovasi rumah atau proyek kecil.",
+    "alamat": "6RWV+R25, Gg. 10, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/EcoukbY5eSWAbcz56",
+    "gambar": ["ilham1.jpg", "ilham2.jpg"]
+  },
+  {
+    "id": 11,
+    "nama": "Ainun 2 Digital Printing & Copy Centre",
+    "kategori": ["Toko & Jasa"],
+    "deskripsi": "Tempat print dan cetak lengkap untuk dokumen, banner, atau kebutuhan desain lainnya. Cepat dan profesional.",
+    "alamat": "Depan SMK Rus, Jl. Bae-Besito, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/j3PyT7B9Sv6BKKkJ8",
+    "gambar": ["ainun1.jpg", "ainun2.jpg"]
+  },
+  {
+    "id": 12,
+    "nama": "Toko Listrik Sinar Terang",
+    "kategori": ["Toko & Jasa"],
+    "deskripsi": "Menjual perlengkapan listrik lengkap, mulai dari kabel, lampu, hingga alat elektronik kecil. Praktis untuk kebutuhan rumah.",
+    "alamat": "Gg. 10, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/Apmdm8PBXXQStV1B8",
+    "gambar": ["sinar1.jpg", "sinar2.jpg"]
+  },
+  {
+    "id": 13,
+    "nama": "Nafisa Parfume 2",
+    "kategori": ["Toko & Jasa"],
+    "deskripsi": "Toko parfum custom, kamu bisa meracik wangi favorit sendiri. Cocok buat hadiah atau koleksi pribadi.",
+    "alamat": "Jl. Besito - Gebog No.80-516, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/TuhWK4rBk9D86SAt5",
+    "gambar": ["nafisa1.jpg", "nafisa2.jpg"]
+  },
+  {
+    "id": 14,
+    "nama": "WEDRINK Kudus",
+    "kategori": ["Minuman", "Makanan"],
+    "deskripsi": "Minuman segar ala Mixue yang hits. Cocok buat melepas dahaga di siang hari dengan rasa manis dan menyegarkan.",
+    "alamat": "Gg. 10, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/PEBBH74SGv7osyT8A",
+    "gambar": ["wedrink1.jpg", "wedrink2.jpg"]
+  },
+  {
+    "id": 15,
+    "nama": "Alin Shop",
+    "kategori": ["Hobi & Jasa"],
+    "deskripsi": "Tempat biliard seru dan nyaman. Cocok buat refreshing dan main bareng teman-teman.",
+    "alamat": "6RXV+353, Jl. Raya Jurang, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "6RXV+353, Jl. Raya Jurang, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "gambar": ["alin1.jpg", "alin2.jpg"]
+  },
+  {
+    "id": 16,
+    "nama": "Campaign Coffee",
+    "kategori": ["Minuman", "Makanan"],
+    "deskripsi": "Kopi nikmat dan tempat nongkrong seru. Pas buat ngobrol santai atau kerja sambil menikmati suasana cozy.",
+    "alamat": "Jl. Besito - Gebog No.80-516, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/KJVwHUJyuvjz1R8SA",
+    "gambar": ["campaign1.jpg", "campaign2.jpg"]
+  },
+  {
+    "id": 17,
+    "nama": "Basina Food",
+    "kategori": ["Makanan", "Minuman"],
+    "deskripsi": "Tempat makan lengkap dengan berbagai menu lezat. Pilihan tepat buat makan siang bareng teman atau keluarga.",
+    "alamat": "Jl. Bae-Besito No.82, Besito Kulon, Besito, Kec. Gebog, Kabupaten Kudus, Jawa Tengah 59333",
+    "maps": "https://maps.app.goo.gl/PBAsr3K8PQJtBhnKA",
+    "gambar": ["basina1.jpg", "basina2.jpg"]
+  }
+]
